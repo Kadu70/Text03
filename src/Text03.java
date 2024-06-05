a7ef5253f99b35188e21366643d594f744f04b1e
ALUNO <ALUNO@LAB04-PC04>
2024-06-04 21:43:54 -0300

    Java_Foundations

---
+++ src/Text03.java
@@ -0,0 +1,25 @@
+public class Text03 {
+    
+    //metodo para patas
+    public static void drawLegs(){
+        System.out.println("     ||     ||      ");
+        System.out.println("     ||     ||      ");
+        System.out.println("    (||)   (||)     ");
+    }
+    
+    
+    //metodo principal
+    public static void main(String[] args) {
+        System.out.println("   /\\         /\\   ");
+        System.out.println("  /  \\_______/  \\  ");
+        System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+        
+        drawLegs();//chama o metodo para desenhar as patas
+
+    }    
+}
